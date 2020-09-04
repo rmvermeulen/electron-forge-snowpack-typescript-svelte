19b861290ff5b96fdf3cd9261395c306b474ae7f
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2020-09-04 13:43:30 +0200

    Added typescript

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
@@ -14,7 +15,7 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, '../index.html'));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
